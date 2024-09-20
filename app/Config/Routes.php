7d127a2cd824d7b752c4541713af5591565f7efc
Taisa15 <taisa.tomlinson@gmail.com>
2024-09-20 00:25:27 -0600

    se agrego editar y actualizar

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -10,3 +10,12 @@ $routes->get('perfume', 'PerfumeController::index');
 
 //create
 $routes->post('createPerfume', 'PerfumeController::create');
+
+//delete
+$routes->delete('deletePerfume/(:num)', 'PerfumeController::delete/$1');
+
+//update
+$routes->post('updatePerfume/(:num)', 'PerfumeController::update/$1');
+
+//findbyid
+$routes->get('perfume/(:num)', 'PerfumeController::findById/$1');
--- app/Controllers/PerfumeController.php
+++ app/Controllers/PerfumeController.php
@@ -47,5 +47,64 @@ class PerfumeController extends BaseController{
         return "Método no permitido.".$this->request->getMethod();
     }
 
+ 
+        public function delete($id)
+    {
+        $perfumeModel = new perfumeModel();
+
+        // Intentar eliminar el registro
+        if ($perfumeModel->delete($id)) {
+            return "Perfume eliminado correctamente.";
+        } else {
+            return "Error al eliminar el perfume.";
+        }
     }
+
+    public function update($id)
+    {
+        if ($this->request->getMethod() === 'POST') {
+            $nombre = $this->request->getPost('nombre'); 
+            $descripcion = $this->request->getPost('descripcion'); 
+            $ml = $this->request->getPost('ml'); 
+            $precio = $this->request->getPost('precio'); 
+
+            $perfumeModel = new PerfumeModel();
+
+            // Preparar los datos para actualizar
+            $data = [
+                'nombre' => $nombre,
+                'descripcion' => $descripcion,
+                'ml' => $ml,
+                'precio' => $precio
+            ];
+
+            // Actualizar el registro
+            if ($perfumeModel->update($id, $data)) {
+                return "Perfume actualizado correctamente.";
+            } else {
+                return "Error al actualizar el perfume.";
+            }
+        }
+
+        return "Método no permitido.";
+    }
+
+    public function findById($id)
+    {
+        $perfumeModel = new PerfumeModel();
+        
+        // Obtener el perfume por ID
+        $perfume = $perfumeModel->find($id);
+        
+        if ($perfume) {
+            // Si se encuentra el perfume, puedes devolverlo a una vista
+            var_dump($perfume);
+        } else {
+            return "Perfume no encontrado.";
+        }
+    }
+
+    }
+
+    
 
